f078c6b181d8d8392f7d330f482607747bf7f9bc
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-09-25 12:56:35 +0200

    Pequeña corrección en el settings

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps locales
+    'bases',
 ]
 
 MIDDLEWARE = [
@@ -62,8 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # apps locales
-                'bases',
             ],
         },
     },
