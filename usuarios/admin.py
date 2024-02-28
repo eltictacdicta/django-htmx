d3f58f6c8d6664f29ced1ecc542d2d3692a157d8
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2024-02-28 18:28:01 +0100

    Creamos el customUser

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+UsuarioPersonalizado = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = UsuarioPersonalizado
+    list_display = ['email', 'username','is_superuser']
+
+admin.site.register(UsuarioPersonalizado, CustomUserAdmin)
